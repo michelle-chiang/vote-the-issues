86902527518de8aa5b58cabca87079157b1e6dac
michelle-chiang <michellechiang88@gmail.com>
2018-10-06 19:31:41 -0400

    create production build

---
+++ build/precache-manifest.836ff4b30c50f4b87162fca44680eb5a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e227ffb742dc57f8a4e7",
+    "url": "/static/js/main.e227ffb7.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "e227ffb742dc57f8a4e7",
+    "url": "/static/css/main.3aaeebc8.chunk.css"
+  },
+  {
+    "revision": "bcbcda815b9303fff72a6b5d4b265dc9",
+    "url": "/index.html"
+  }
+];
