9c85ec8f4f7d090e6e3ae574b3ffa189db2c7df3
michelle-chiang <michellechiang88@gmail.com>
2018-10-12 16:12:41 -0400

    update build and deploy

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4d629679224471cd345826b22eeed58e.js"
+  "/precache-manifest.877748bced4f8d3a3369e89abd32d1b3.js"
 );
 
 workbox.clientsClaim();
