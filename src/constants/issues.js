79dd4f5da0c3d5445cea989cc191aea2b94414d6
Oliver <yorkolivers@gmail.com>
2018-10-22 11:30:23 -0400

    More issues added

--- src/constants/issues.js
+++ src/constants/issues.js
@@ -1,18 +1,28 @@
 const issueData = {
+    'Trump': [
+        'Congressional Dems need the power to subpoena this corrupt administration.',
+        '...',
+        '...',
+    ],
     'Conservation': [
-        'description 1 that is really really long',
-        'description 2',
-        'description 3'
+        'National monuments are public treasures, not business opportunities for Trump’s mining buddies.',
+        'The DoD believes in climate change. So should Congress.',
+        'Remember the polar bears? Only Congress can save the Artic National Wildlife Refuge from oil drilling.'
     ],
-    'Category 2': [
-        'issue 1',
-        'issue 2',
-        'issue 3'
+    'Voting Rights': [
+        'It\'s time for the Voting Rights Act of 2018.',
+        'The culture wars are back and we can’t let neo-Nazis gain an inch.',
+        'Flint still doesn’t have reliable, lead-free drinking water.'
     ],
-    'Category 3': [
-        'hi 1',
-        'hi 2',
+    'Hey Congress -- Get Your Act Together!': [
+        'Weapons of war have no place in schoolhouse or police stations.',
+        'Congress shouldn\'t ransom undocumented children to buy Trump a wall.',
         'hi 3'
+    ],
+    'Students': [
+        'Education is forever. Student loans shouldn\'t be.',
+        'We marched together. Now it\'s time to vote together.',
+        '...',
     ]
 }
 
