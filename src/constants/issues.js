054c8f77314051187a58037d731e65a4f7ff5a2f
Oliver <yorkolivers@gmail.com>
2018-10-28 21:19:23 -0400

    Adding issues text

--- src/constants/issues.js
+++ src/constants/issues.js
@@ -1,23 +1,28 @@
 const issueData = {
     'Trump': [
         'Congressional Dems need the power to subpoena this corrupt administration.',
-        '...',
-        '...',
-    ],
-    'Conservation': [
-        'National monuments are public treasures, not business opportunities for Trump’s mining buddies.',
-        'The DoD believes in climate change. So should Congress.',
-        'Remember the polar bears? Only Congress can save the Artic National Wildlife Refuge from oil drilling.'
-    ],
-    'Voting Rights': [
-        'It\'s time for the Voting Rights Act of 2018.',
-        'The culture wars are back and we can’t let neo-Nazis gain an inch.',
-        'Flint still doesn’t have reliable, lead-free drinking water.'
+        'Mueller needs Congressional protection so he can get his job done fairly.',
+        'There were not "some very fine people on both sides," and I don’t want my representative to be an enabler of Trump’s bigotry.',
     ],
     'Hey Congress — Get Your Act Together!': [
         'Weapons of war have no place in schoolhouse or police stations.',
-        'Congress shouldn\'t ransom undocumented children to buy Trump a wall.',
-        'hi 3'
+        'Congress shouldn’t ransom undocumented children to buy Trump a wall.',
+        'The national debt is greater than GDP, and Congress is still giving tax cuts to the rich?'
+    ],
+    'Conservation': [
+        'National monuments are public treasures, not business opportunities for Trump’s mining buddies.',
+        'The Department of Defense believes in climate change. So should Congress.',
+        'Remember the polar bears? Only Congress can save the Arctic National Wildlife Refuge from oil drilling.'
+    ],
+    'Health Care': [
+        'Universal health care is a human right. Congressional Republicans still haven’t gotten the message.',
+        'People with pre-existing conditions need more support, not less. (Looking at you, Congress.)',
+        ''
+    ],
+    'Voting Rights': [
+        'It’s time for the Voting Rights Act of 2018.',
+        'The culture wars are back and we can’t let neo-Nazis gain an inch.',
+        'Flint still doesn’t have reliable, lead-free drinking water.'
     ],
     'Students': [
         'Education is forever. Student loans shouldn\'t be.',
