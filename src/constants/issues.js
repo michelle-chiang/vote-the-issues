f955ad525e75de64fd4e778e75d3de45f04f8392
Oliver <yorkolivers@gmail.com>
2018-10-28 21:36:57 -0400

    Added issues text

--- src/constants/issues.js
+++ src/constants/issues.js
@@ -14,20 +14,30 @@ const issueData = {
         'The Department of Defense believes in climate change. So should Congress.',
         'Remember the polar bears? Only Congress can save the Arctic National Wildlife Refuge from oil drilling.'
     ],
-    'Health Care': [
+    'Healthcare': [
         'Universal health care is a human right. Congressional Republicans still haven’t gotten the message.',
         'People with pre-existing conditions need more support, not less. (Looking at you, Congress.)',
-        ''
+        'The Affordable Care Act... works. Why is Congress so eager to repeal it?'
     ],
-    'Voting Rights': [
+    'Economic Sanity': [
+        'We need a livable federal minimum wage.',
+        'Cutting taxes on the wealthy is just plain illogical.'.
+        'It’s okay to be jealous of California, but eliminating the state tax deduction is just mean-spirited.'
+    ],
+    'Civil Rights': [
         'It’s time for the Voting Rights Act of 2018.',
         'The culture wars are back and we can’t let neo-Nazis gain an inch.',
         'Flint still doesn’t have reliable, lead-free drinking water.'
     ],
+    'Criminal Justice': [
+        'Jail time shouldn’t depend on whether or not you can pay bail.',
+        'Felony disenfranchisement is racist, partisan, and mean-spirited.',
+        'Mass incarceration is still shredding my community.'
+    ],
     'Students': [
-        'Education is forever. Student loans shouldn\'t be.',
-        'We marched together. Now it\'s time to vote together.',
-        '...',
+        'Education is forever. Student loans shouldn’t be.',
+        'We marched together. Now it’s time to vote together.',
+        'My future is online. We need net neutrality to keep corporate monopolies out of the way.'
     ]
 }
 
